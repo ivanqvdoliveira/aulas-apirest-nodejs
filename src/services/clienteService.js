9138c243311141ab065aa11b876961663c62d4f8
Ivan Oliveira <ivanaugoli@hotmail.com>
2022-04-08 21:33:46 -0300

    aula 20 - metodo par atualizar o cliente

--- src/services/clienteService.js
+++ src/services/clienteService.js
@@ -28,6 +28,25 @@ async function cadastrar (clienteDTO) {
 }
 
 async function atualizar (clienteDTO) {
+  clienteDTO.id = parseInt(clienteDTO.id)
+  let cliente  = await Cliente.findByPk(clienteDTO.id)
+
+  if (!cliente) {
+    throw new NaoEncontradoErro(404, `Não foi possível encontrar um cliente com o id ${clienteDTO.id}`)
+  }
+  
+  let atualiado = await Cliente.update(clienteDTO, { qhere: { id: clienteDTO.id }})
+
+  if (!atualiado) {
+    throw new AplicacaoErro(500, 'Não foi possível atualizar o cliente')
+  }
+
+  for (let endereco in clienteDTO.enderecos) {
+    await Endereco.update(endereco, { where: { id: endereco.id }})
+  } // vai pegar a lista de endereços e pra cada um deles vai atualizar no banco, pois não precisa inserir novmamente no DTo pois já está lá e está configurado, ainda não está tratando se por algum motivo o endereço estiver fora dos padrões
+
+  return clienteDTO
+
 
 }
 
